a71e4c5a335c0c8bfa94525255bbb371fc4dfe39
ist197969 <enrygiorio97@gmail.com>
2022-03-25 21:20:59 +0000

    Debugged ibmparser

--- ibmparser.py
+++ ibmparser.py
@@ -5,18 +5,19 @@ import re
 objects = dict()  # key: object_id -> value: (size, tp_read, tp_write)
 min_ts = inf
 max_ts = 0
-pattern = re.compile("^[0-9]+ (\w)+\.(\w)+\.(\w)+ [0-9]+( [0-9]+ [0-9]+)?$")
+pattern = re.compile("^[0-9]+ (\w)+\.(\w)+\.(\w)+ [0-9]+( [0-9]+ [0-9]+)?")
 with open("IBMObjectStoreTrace000Part0", "r") as file, open(
     "traces.txt", "w"
 ) as output:
     for line in file:
         if not re.match(pattern, line):
+            # print("line doesn't match")
             continue
 
         line = line.split()
-
-        if len(line) < 4:
-            continue
+        # print(f"parsing {line}")
+        #        if len(line) < 4:
+        #           continue
 
         try:
             ts = int(line[0])
@@ -26,7 +27,7 @@ with open("IBMObjectStoreTrace000Part0", "r") as file, open(
         op = line[1].split(".")[1]  # REST.GET.OBJECT -> GET
         object_id = line[2]
         object_size = line[3]
-
+        # print(f"Found object {object_id}")
         if object_id in objects:
             value = objects[object_id]
             if op == "GET" or op == "HEAD":  # READ
@@ -37,6 +38,7 @@ with open("IBMObjectStoreTrace000Part0", "r") as file, open(
             objects[object_id] = new_value
 
         else:
+            # print(f"First seen, op={op}")
             if op == "GET" or op == "HEAD":  # READ
                 objects[object_id] = (object_size, 1, 0)
             else:
@@ -47,9 +49,10 @@ with open("IBMObjectStoreTrace000Part0", "r") as file, open(
         if ts < min_ts:
             min_ts = ts
 
-        time = max_ts - min_ts
+    time = max_ts - min_ts
+    # print(f"{objects}")
 
-    for size, tp_r, tp_w in objects.values():
-        output.write(f"{size} {tp_r} {tp_w}\n")
+    for (size, tp_r, tp_w) in objects.values():
+        output.write(f"{size} {tp_r/time:.3f} {tp_w/time:.3f}\n")
 
-    output.truncate(output.tell() - 1)  # remove trailing newline
+#    output.truncate(output.tell() - 1)  # remove trailing newline
