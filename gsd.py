680dbd232a42aa57fc5fce9829306a48369419a9
ist197969 <enrygiorio97@gmail.com>
2022-05-23 20:43:12 +0100

    skew 2 in gsd script

--- gsd.py
+++ gsd.py
@@ -7,7 +7,7 @@ N = 10000
 max_tp = 200
 items_size = 0.1
 prettyprint = "01"
-skew = 1
+skew = 2
 while skew <= 4:
     while max_tp <= 2000:
         while items_size <= 1000:
@@ -17,10 +17,6 @@ while skew <= 4:
             os.system(
                 f"python3 ilp_solve_clustersize.py {N} {items_size:.1f} {max_tp} zipfian {skew} results/{skew}/{N}_{int(items_size) if items_size >= 1 else prettyprint}MB_{max_tp}IOPS.txt"
             )
-            # text = f"Optimisation finished: 10000 items, sizes={items_size}MB, tot tp = {max_tp}IO/s"
-            # requests.get(
-            #     f"https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={text}"
-            # )
             items_size *= 10
         max_tp *= 10
     skew += 1
