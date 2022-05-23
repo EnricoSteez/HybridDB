7e128f2e2e2e04b6993ab3e234f554257a8416a6
ist197969 <enrygiorio97@gmail.com>
2022-05-23 16:05:26 +0100

    Improved script

--- gsd.py
+++ gsd.py
@@ -6,15 +6,16 @@ chat_id = "907706827"
 N = 10000
 max_tp = 200
 items_size = 0.1
+prettyprint = "01"
 skew = 1
 while skew <= 4:
     while max_tp <= 2000:
         while items_size <= 1000:
             print(
-                f"Starting optimisation with N={N}, size={items_size:.1f}, max_tp={max_tp}, distribution: zipfian (skew={skew})"
+                f"Starting optimisation with N={N}, size={int(items_size) if items_size >= 1 else prettyprint}, max_tp={max_tp}, distribution: zipfian (skew={skew})"
             )
             os.system(
-                f"python3 ilp_solve.py {N} {items_size:.1f} {max_tp} zipfian {skew} results/{N}_{items_size}MB_{max_tp}IOPS_{skew}.txt"
+                f"python3 ilp_solve.py {N} {items_size:.1f} {max_tp} zipfian {skew} results/{skew}/{N}_{items_size}MB_{max_tp}IOPS.txt"
             )
             # text = f"Optimisation finished: 10000 items, sizes={items_size}MB, tot tp = {max_tp}IO/s"
             # requests.get(
