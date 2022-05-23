6e2f07930836cb1bc2f8b5dda25c5ac692c225c5
ist197969 <enrygiorio97@gmail.com>
2022-05-23 15:28:44 +0100

    launch script fix

--- gsd.py
+++ gsd.py
@@ -3,14 +3,18 @@ import os
 
 api_key = "5187998346:AAFPwXQsNR1EQi2e7osOswEsFHAjDxiTpMk"
 chat_id = "907706827"
+N = 10000
 max_tp = 200
 items_size = 0.1
 skew = 1
 while skew <= 4:
     while max_tp <= 2000:
         while items_size <= 1000:
+            print(
+                f"Starting optimisation with N={N}, size={items_size:.1f}, max_tp={max_tp}, distribution: zipfian (skew={skew})"
+            )
             os.system(
-                f"python3 ilp_solve.py 10000 ${items_size} ${max_tp} zipfian {skew} results/${items_size}MB_${max_tp}IOPS_{skew}.txt"
+                f"python3 ilp_solve.py {N} {items_size:.1f} {max_tp} zipfian {skew} results/{N}_{items_size}MB_{max_tp}IOPS_{skew}.txt"
             )
             # text = f"Optimisation finished: 10000 items, sizes={items_size}MB, tot tp = {max_tp}IO/s"
             # requests.get(
