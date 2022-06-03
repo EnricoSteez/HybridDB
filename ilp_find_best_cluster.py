d962f966e13368f7aac6796c740bc1693a64e6ad
ist197969 <enrygiorio97@gmail.com>
2022-06-03 19:12:28 +0100

    Writing to separate file only hybrid findings

--- ilp_find_best_cluster.py
+++ ilp_find_best_cluster.py
@@ -362,9 +362,12 @@ print(
 print("-" * 80)
 best_no_hybrid = min(cost_dynamo, best_cost_cassandra)
 cost_hybrid = problem.objective.value()
-print(f"Cost saving compared to best option: {best_no_hybrid-cost_hybrid:.2f} €/h")
+saving = best_no_hybrid - cost_hybrid
+print(f"Cost saving compared to best option: {saving/best_no_hybrid:.2%} €/h")
 if best_no_hybrid - cost_hybrid != 0:
-    print(f"Cost savinf percentage: {(best_no_hybrid-cost_hybrid)/best_no_hybrid:.2f}")
+    print(f"Cost savinf percentage: {saving/best_no_hybrid:.2f}")
+    with open("results/hybridScenarios.txt", "wa") as file:
+        file.write(f"{filename} -> {saving/best_no_hybrid:.2%}\n")
 
 tot_time = time() - t0
 print(f"Took {tot_time:.2f} seconds ")
