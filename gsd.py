0a1c5567049d3f62f7b64446a50bcac4cb1d3567
ist197969 <enrygiorio97@gmail.com>
2022-06-04 00:32:01 +0100

    Added optimisations count for debugging

--- gsd.py
+++ gsd.py
@@ -11,6 +11,7 @@ token = "5187998346:AAFPwXQsNR1EQi2e7osOswEsFHAjDxiTpMk"
 chat_id = "907706827"
 N = 1000
 t0 = time()
+count = 0
 for percent_read in [0.9, 0.95, 0.99]:
     for skew in [1, 2, 3, 4]:
         for tot_tp in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
@@ -21,6 +22,7 @@ for percent_read in [0.9, 0.95, 0.99]:
                     f"reads: {percent_read:.1%}",
                 )
                 print(message)
+                count += 1
                 if (
                     os.system(
                         f"python3 ilp_find_best_cluster.py {N} {items_size} {tot_tp} zipfian {skew} {percent_read}"
@@ -29,7 +31,7 @@ for percent_read in [0.9, 0.95, 0.99]:
                 ):
                     sys.exit("Error, stopping")
 tot_time = time() - t0
-message = f"Tutto finito, it took {strftime('%H:%M:%S',gmtime(tot_time))}!"
+message = f"Tutto finito! Did {count} optimisations, it took me {strftime('%H:%M:%S',gmtime(tot_time))}!"
 requests.get(
     f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={urllib.parse.quote(message)}&disable_notification=True"
 )
