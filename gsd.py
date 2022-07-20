3d2a9743cf144c4ac765c0b6c92f5dc2c192bbfe
ist197969 <enrygiorio97@gmail.com>
2022-07-20 11:07:10 +0100

    "Better cleanup at gsd script launch"

--- gsd.py
+++ gsd.py
@@ -13,6 +13,7 @@ count = 0
 script_name = sys.argv[1]
 os.system("rm ../results/*")
 os.system("rm ../placements/*")
+os.system("rm ../workloads.xlsx")
 t0 = time()
 for percent_read in [0.9, 0.95, 0.99, 0.1, 0.5]:
     for skew in [1, 2, 3, 4]:
