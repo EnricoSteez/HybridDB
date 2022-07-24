b79532a373b9c19c74dd5e6b55b47e4fe04bed3c
ist197969 <enrygiorio97@gmail.com>
2022-07-24 19:58:20 +0100

    "Script for running merged workloads"

--- manual.py
+++ manual.py
@@ -8,7 +8,6 @@ from time import strftime
 
 token = "5187998346:AAFPwXQsNR1EQi2e7osOswEsFHAjDxiTpMk"
 chat_id = "907706827"
-N = 1000
 count = 0
 script_name = sys.argv[1]
 os.system("rm ../results/*")
@@ -17,24 +16,24 @@ os.system("rm ../placements/*")
 os.system("mkdir ../placements")
 os.system("rm ../workloads.xlsx")
 t0 = time()
-for percent_read in [0.1]:
-    for skew in [1,2]:
-        for tot_tp in [1,10,100,1000]:
-            for items_size in [1,10,100,1000]:
-                message = (
-                    f"Starting optimisation: N={N}, items_size={items_size}, ",
-                    f"tot_tp={tot_tp}, distribution: zipfian (skew={skew})",
-                    f"reads: {percent_read:.1%}",
-                )
-                print(message)
-                count += 1
-                if (
-                    os.system(
-                        f"python3 {script_name} {N} {items_size} {tot_tp} zipfian {skew} {percent_read}"
-                    )
-                    != 0
-                ):
-                    sys.exit("Error, stopping")
+N = 1000
+skew = 2
+for percent_read1 in [0.5, 0.95]:
+    for tot_tp1 in [0.1, 100]:
+        for items_size1 in [0.0001, 1]:
+            for percent_read2 in [0.5, 0.95]:
+                for tot_tp2 in [0.1, 100]:
+                    for items_size2 in [0.0001, 1]:
+                        count += 1
+                        if (
+                            os.system(
+                                f"python3 ilp_no_dynamo_two_zipfians.py "
+                                f"{N} {items_size1} {tot_tp1} {skew} {percent_read1} "
+                                f"{N} {items_size2} {tot_tp2} {skew} {percent_read2} "
+                            )
+                            != 0
+                        ):
+                            sys.exit("Error, stopping")
 tot_time = time() - t0
 message = f"Tutto finito! Did {count} optimisations, it took me {strftime('%H:%M:%S',gmtime(tot_time))}!"
 requests.get(
